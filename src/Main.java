24f53ff92c78fe2f72bc6c5250f3826437e56576
yxy <546752505@qq.com>
2019-10-20 11:12:54 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import cn.yxy.test.domain.BBuser;
+import cn.yxy.test.domain.User;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+ final  static  String ss = "aaa";
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        HashMap<Object, Object> map = new HashMap<>();
+
+        Arrays.asList();
+
+        test("aaa");
+        User user = new User();
+        user.setName("");
+        user.setSex("");
+        user.setNo("");
+        user.setWeight("");
+//        user.setAge(0);
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+
+        String json="{\"name\":\"aaaa\",\n" +
+                "\"goods\":\"bbbb\"\n" +
+                "}";
+        System.out.println("Hello World!");
+    }
+
+
+    public  static  String test(String ss ){
+        System.out.println("------>"+ss);
+        return  ss;
+    }
+    public  static  String test(String ss,String aa ){
+        System.out.println("------>"+ss);
+        return  ss;
+    }
+
+    public BBuser conver(User user){
+        BBuser bBuser = new BBuser();
+        bBuser.setName(user.getName());
+        bBuser.setWeight(user.getWeight());
+        return bBuser;
+
+    }
+
+    public BBuser test(User user){
+        BBuser bBuser = new BBuser();
+        bBuser.setName(user.getName());
+        bBuser.setWeight(user.getWeight());
+        return bBuser;
+
+
+    }
+}
---
+++ src/cn/yxy/test/domain/User.java
@@ -0,0 +1,60 @@
+package cn.yxy.test.domain;
+
+public class User {
+
+
+
+    private String name;
+
+
+    private String sex;
+
+
+    private String no;
+
+
+    private String weight;
+
+
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
